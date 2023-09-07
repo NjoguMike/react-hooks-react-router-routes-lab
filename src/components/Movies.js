ec8f2eba8236ccefc61a9430958a829d1b1d3382
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-09-07 13:09:10 +0300

    Complete Navigation lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React  from "react";
+// import { v5 as id } from "uuid"
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // const keyId = id()
+  const movie = movies.map(movie => <div key={movie.title}>
+    <h3>Name: {movie.title}</h3>
+    <p>Time: {movie.time}</p>
+    <ul>{movie.genres.map(genre => <li key={genre + movie.time}>{genre}</li>)}</ul>
+    </div>)
+      
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movie}
+    </div>
+  );
 }
 
 export default Movies;
